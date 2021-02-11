74aa24aa8e1950d48885408f521dc395a1b0b9cd
Paulin Jouhet <paul.jouh@gmail.com>
2021-02-11 16:40:08 +0100

    maj paulin

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-<<<<<<< HEAD
-import { StyleSheet, View } from 'react-native';
-
-=======
 import { StyleSheet, View, Button, TouchableOpacity} from 'react-native';
->>>>>>> 6b690bc33237dcf2383e1e5299aadc52a6bc2785
 // jimporte le perso principale qui se situe dans les composants
 
 import Night from './assets/images/night.jpg'
@@ -13,18 +8,6 @@ import  Oiseau from './assets/components/Oiseau'
 import  Sol from './assets/components/Sol'
 import  Ennemi from './assets/components/Ennemi'
 
-//test
-
-
-   return {
-   height: 100,
-   width: 50,
-   marginTop: 10,
-   backgroundColor : "red",
-   marginLeft: gauche, }
-}
-
-// fin test
 export default function App() {
 
   const [DeplacementOiseau, setDeplacementOiseau] = useState(0);
