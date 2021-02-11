2c581d0a5af2233a1ec66674772897bb6b6b146a
enzoarnaudet <56151607+enzoarnaudet@users.noreply.github.com>
2021-02-11 15:40:44 +0100

    byby game

--- assets/components/Oiseau.js
+++ assets/components/Oiseau.js
@@ -3,17 +3,19 @@ import { View } from 'react-native';
 import Bird from '../images/Bird.png'
 // le view remplace une div, ici je viens juste créer mon composant oiseau à l'aide d'une constante qui est donc en faite le perso principal
 
-const Oiseau = () => {
+const Oiseau = ({DeplacementOiseau}) => {
     return (
         // La div (view) permet de gérer directement le style de notre composant soit ici le carré bleu
         <View style={{
             position: 'absolute',
             bottom: 300,
-            left: 50,
+            Right: DeplacementOiseau,
             width:100,
             height: 100,
            background: `url(${Bird})`
         }}/>
+
+        
 
     )
 }
