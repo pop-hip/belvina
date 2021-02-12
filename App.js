416804c347429ee18f3ffa4558f367f2d9938e64
enzoarnaudet <56151607+enzoarnaudet@users.noreply.github.com>
2021-02-12 11:33:48 +0100

    gravité

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, View, Button, TouchableOpacity} from 'react-native';
-// jimporte le perso principale qui se situe dans les composants
 
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Button} from 'react-native';
 import Night from './assets/images/night.jpg'
 import  Oiseau from './assets/components/Oiseau'
 import  Sol from './assets/components/Sol'
 import  Ennemi from './assets/components/Ennemi'
+export default function App() { 
+  //Permet de faire la gravité de notre oiseau
+   const [DeplacementOiseau, setDeplacementOiseau] = useState(0);
+   const [HauteurOiseau, setHauteurOiseau] = useState(450);
+
+let gravityTimerId              //variable dans laquelle on implante l'intervale pour la gravité de Bird()
+  
+useEffect(() => {                                  // le useEffect permet de
+  if (HauteurOiseau > 0 ) {                       //L'oiseau ne va pas plus bas que 0
+      gravityTimerId = setInterval(() => {       //on initialise l'intervale entre la hauteur actuelle de l'oiseau -3 pour la gravité de l'oiseau
+      setHauteurOiseau(HauteurOiseau - 3)       //applique la gravité à la positon de Bird()
+    }, 30)                                     //réactualisé toutes les 30ms
+  }
+  return () => {
+    clearInterval(gravityTimerId)
+  }
+}, [HauteurOiseau])
 
 
-export default function App() {
-
-  const [DeplacementOiseau, setDeplacementOiseau] = useState(0);
+ 
   return (
     <View
     style={{
-     
      width: 400,
      height: 810,
      background: `url(${Night})`
@@ -23,13 +35,13 @@ export default function App() {
    }}>
   
      <Button
-     onPress={() =>  setDeplacementOiseau(DeplacementOiseau + 10) }
-     title="Avancer"
-      
+       onPress={() =>  setDeplacementOiseau(DeplacementOiseau + 10) }
+       title="Avancer"
      />
-      <StatusBar style="auto" />
       <Oiseau
-      DeplacementOiseau = {DeplacementOiseau} />
+        DeplacementOiseau = {DeplacementOiseau} 
+        HauteurOiseau = {HauteurOiseau}
+      />
       <Ennemi />
       <Sol />
     </View>
--- assets/components/Oiseau.js
+++ assets/components/Oiseau.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { View } from 'react-native';
 import Bird from '../images/Bird.png'
 // le view remplace une div, ici je viens juste créer mon composant oiseau à l'aide d'une constante qui est donc en faite le perso principal
+// dans la const on a placé deux paramètres qui permettent à l'oiseau d'avancer(DeplacementOiseau) et d'avoir de la gravité(HauteurOiseau)
 
-const Oiseau = ({DeplacementOiseau}) => {
+const Oiseau = ({DeplacementOiseau, HauteurOiseau}) => {
     return (
         // La div (view) permet de gérer directement le style de notre composant soit ici l'oiseau'
-        // J'ai placé en paramètre la const deplacementoiseau utilisé dans l'app js et je l'ai ensuite appélé au left pour que l'oiseau se déplace de 10 en 10 comme l'indique la const deplacementoiseau et tout ça sur la gauche (left)
+        
         <View style={{
             position: 'absolute',
-            bottom:300,
-            left: DeplacementOiseau,
+            bottom: HauteurOiseau,//permet de faire descendre toute les 30ms l'oiseau de 3 px(effet de graviité)
+            left: DeplacementOiseau,// permet de faire avancer l'oiseau de 10 vers la gauche
             width:100,
             height: 100,
-           background: `url(${Bird})`
+            background: `url(${Bird})`
         }}/>
 
         
